491737e69d696f5b26493f953621fb1a9611ab52
Isami Yuma <lemonburst1958@gmail.com>
2020-12-23 15:07:40 +0900

    feat: modify Slider

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,4 +1,17 @@
-import Slider from 'rc-slider';
+import React from 'react';
+import RCSlider from 'rc-slider';
 import 'rc-slider/assets/index.css';
+
+type Props = {
+  value: number;
+  min: number;
+  max: number;
+  step: number;
+  onChange: (val: number) => void;
+};
+
+const Slider: React.FC<Props> = ({ value, min, max, step, onChange }) => (
+  <RCSlider value={value} min={min} max={max} step={step} onChange={onChange} />
+);
 
 export default Slider;
