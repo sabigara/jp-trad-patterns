333c19200f4a39b7b573e2398b7113412fa254f7
Isami Yuma <lemonburst1958@gmail.com>
2020-12-26 13:37:05 +0900

    fix: imports of patterns

--- src/patterns/index.ts
+++ src/patterns/index.ts
@@ -1,8 +1,8 @@
-export { default as Yagasuri } from '@/patterns/Yagasuri';
-export { default as Seigaiha } from '@/patterns/Seigaiha';
-export { default as HishiSeigaiha } from '@/patterns/HishiSeigaiha';
-export { default as Ichimatsu } from '@/patterns/Ichimatsu';
-export { default as Shichihou } from '@/patterns/Shichihou';
-export { default as Matsukawabishi } from '@/patterns/Matsukawabishi';
-export { default as Urokomon } from '@/patterns/Urokomon';
-export { default as Kagome } from '@/patterns/Kagome';
+export { default as Yagasuri } from '@/patterns/src/Yagasuri';
+export { default as Seigaiha } from '@/patterns/src/Seigaiha';
+export { default as HishiSeigaiha } from '@/patterns/src/HishiSeigaiha';
+export { default as Ichimatsu } from '@/patterns/src/Ichimatsu';
+export { default as Shichihou } from '@/patterns/src/Shichihou';
+export { default as Matsukawabishi } from '@/patterns/src/Matsukawabishi';
+export { default as Urokomon } from '@/patterns/src/Urokomon';
+export { default as Kagome } from '@/patterns/src/Kagome';
